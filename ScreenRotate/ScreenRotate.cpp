ad9b977811021f760f03d2ec2621306b474aea45
jayongg <jayongg_2001@hotmail.com>
2017-04-29 18:11:11 -0700

    update to have it work

--- ScreenRotate/ScreenRotate.cpp
+++ ScreenRotate/ScreenRotate.cpp
@@ -13,20 +13,26 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	if (!EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &devmode))
 		return -1;
 
+	LPCTSTR pszCurrent = L"TBD";
+
 	// increment the orientation
 	// determine new orientaion
 	switch (devmode.dmDisplayOrientation)
 	{
 	case DMDO_DEFAULT:
+		pszCurrent = L"default";
 		devmode.dmDisplayOrientation = DMDO_270;
 		break;
 	case DMDO_270:
+		pszCurrent = L"270";
 		devmode.dmDisplayOrientation = DMDO_180;
 		break;
 	case DMDO_180:
+		pszCurrent = L"180";
 		devmode.dmDisplayOrientation = DMDO_90;
 		break;
 	case DMDO_90:
+		pszCurrent = L"90";
 		devmode.dmDisplayOrientation = DMDO_DEFAULT;
 		break;
 	default:
@@ -34,6 +40,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		// add exception handling here
 		break;
 	}
+
+	OutputDebugString(pszCurrent);
+	DWORD dwValue = devmode.dmPelsWidth;
+	devmode.dmPelsWidth = devmode.dmPelsHeight;
+	devmode.dmPelsHeight = dwValue;
 
 	LONG result = ChangeDisplaySettings(&devmode, 0);
 	
